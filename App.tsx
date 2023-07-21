d874cc84d6f9956071db0bd0873bc34e38fb89b6
BereniceQueiroz <baqueiroz14@gmail.com>
2023-07-21 20:44:23 -0300

    feat: config One Signal

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
-import { StatusBar } from 'react-native';
+import { StatusBar, Platform } from 'react-native';
+import OneSignal from 'react-native-onesignal';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
@@ -8,6 +9,15 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+const oneSignalID = Platform.OS === 'ios' ? '' : 'aae6395a-648a-4a37-b4f9-6eb605f4a5a7';
+
+OneSignal.setAppId(oneSignalID);
+
+//para solicitar ao usuario permissao para receber push
+OneSignal.promptForPushNotificationsWithUserResponse(res => {
+  console.log(res);
+}) 
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
